0ce2d5c086de095577206a82e369ba13443eb72d
Maha Uddin <Student@Admins-MacBook-Pro.local>
2025-03-14 17:56:18 -0400

    specified the 3000 port and used the express's listen() function

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ import "dotenv/config";
 // Create an instance of express
 const requestHandler = express();
 // Storing our port value from the .env file
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // The middleware express.json() parses incoming JSON requests and puts the parsed data in req.body
 requestHandler.use(express.json());
+import "dotenv/config"
+// Starting our http server and listening for requests!
+requestHandler.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+
+  
+
+
+
+
+
