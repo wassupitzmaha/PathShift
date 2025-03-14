6a9f2597fc8d588a0cc4f91e6d84c9bc0280fbdc
Maha Uddin <Student@Admins-MacBook-Pro.local>
2025-03-14 17:42:20 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// We use express to create our servers endpoints, and listen for & respond to requests from the frontend
+import express from "express";
+// We use dotenv so that we can access our environment variables
+import "dotenv/config";
+
+// Create an instance of express
+const requestHandler = express();
+// Storing our port value from the .env file
+const port = process.env.PORT;
+
+// The middleware express.json() parses incoming JSON requests and puts the parsed data in req.body
+requestHandler.use(express.json());
